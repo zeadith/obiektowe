2264be67aeaece914c3a9385e5b005c9c5981331
Katarzyna Zet <zeadith@gmail.com>
2016-03-09 12:12:55 +0100

    Long overdue update with changes from class.

--- user.js
+++ user.js
@@ -47,3 +47,53 @@ var michal = new User('michal.matulka@protonmail.ch', 'lla123', 'http://awatary.
 
 userList.addUser(zosia);
 userList.addUser(michal);
+
+
+//zadanie2
+/*Stwórz nowy konstruktor "UserView", którego argumentem będzie instancja User
+W konstruktorze UserView wygeneruj element li, zawierający obrazek z awatarem użytkownika, oraz jego adres e-mail umieszczony jako link z protokołem "mailto:" (nadaj tym elementom jakieś ładne style :) ). Przypisz element dom oraz obiekt User jako własności tworzonego obiektu.
+W prototypie UserView utwórz metodę refresh odświeżającą dane użytkownika w elemencie dom.*/
+
+
+var UserView = function (user) { //konstruktor
+//tworzony element listy li, 
+//element img, element p, przypisujemy jako dzieci li
+
+this.elem = document.createElement('li');
+this.avatarImg = document.createElement('img');
+this.mailLink = document.createElement('a');
+//nie 'var lista'bo to wlasnosc mojego obiektu
+this.user = user;  // przypisujemy naszego usera do wlasnosci konstruktora
+this.elem.appendChild(this.avatarImg);
+this.elem.appendChild(this.mailLink);
+};
+
+//w metodzie refresh wrzucamy to dopiero. 
+
+var showUsers = new UserView (user); //nowy obiekt
+
+UserView.prorotype.refresh = function (){
+  this.avatarImg.src = this.user.avatarUrl;
+  this.mailLink.href = this.user.email;
+  this.mailLink.innerHTML = //
+}; 
+
+var list = document.getElementsByTagName('ul')[0];
+for (var i = 0; i < userList.allUsers.length; i++) {
+    var userView = new UserView(userList.allUsers[i]);
+    list.appendChild(userView)
+}
+
+//ZADANIE 3
+//Rozszerz standardową tablicę JavaScript o metodę sum(), 
+//która zwróci wynik dodawania wszystkich elementów tablicy.
+
+Array.prototype.sum = function(){
+    var suma = 0;
+    for (var i =0; i< this.length; i++){
+      suma = suma + this[i];
+    }
+    return suma;
+};
+
+//metoda sum jest dostepna dla kazdego tworzonego arraya
